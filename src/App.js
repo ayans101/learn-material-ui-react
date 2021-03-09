280f10727b7f59c3f7efa8720adab866b5678668
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-10 01:35:40 +0530

    add material ui button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup color="primary" variant='contained'>
+          <Button 
+            startIcon={<SaveIcon />}
+            size="small"
+            // variant="contained" 
+            // color="primary"
+          >
+            Save
+          </Button>
+          <Button 
+            startIcon={<DeleteIcon />}
+            size="small"
+            // variant="contained" 
+            // color="secondary"
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
